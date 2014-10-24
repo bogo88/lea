417c00d73952360d8f8b05dad56c1b71f2f7a837
Boguslaw Szczuka <you@example.com>
2014-10-24 17:58:43 +0200

    ratings

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Meal, User, Rating
 
-# Register your models here.
+admin.site.register(Meal)
+admin.site.register(User)
+admin.site.register(Rating)
