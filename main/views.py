a6fbca5808ebf391c2f7e62c8a9d59062f57704b
Boguslaw Szczuka <you@example.com>
2014-10-27 16:21:02 +0100

    Token auth test

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from main.forms import RateForm
 
-from main.models import Meal, Rating
+from main.models import Meal, Rating, User
 
 
 class IndexView(generic.ListView):
@@ -29,5 +29,14 @@ def rating(request, meal_id):
 
 def rate(request, meal_id):
     meal = get_object_or_404(Meal, pk=meal_id)
+    user = get_object_or_404(User, pk=1)
+    model = Rating()
+    if request.POST:
+        model.meal = meal
+        model.user = user
+        model.value = request.POST.get('value')
+        model.comment = request.POST.get('comment')
+        model.save()
+
     form = RateForm()
     return render(request,'main/rate.html',{'meal': meal,'form': form})
