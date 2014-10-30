d366c6b1a806f6bc58f1a57d6f613c02b4c99566
Boguslaw Szczuka <you@example.com>
2014-10-30 14:37:34 +0100

    requirements added

--- lea/settings.py
+++ lea/settings.py
@@ -91,5 +91,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = '/var/www/lea/main/static'
 STATIC_URL = '/static/'
