1aa188e3a8a127b3002de8507cb9742a642f4cb0
Boguslaw Szczuka <you@example.com>
2014-10-30 14:27:20 +0100

    requirements added

--- lea/urls.py
+++ lea/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-
+from lea import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^', include('main.urls',namespace='main')),
                        url(r'^admin/', include(admin.site.urls)),
-                       )
+                      )
