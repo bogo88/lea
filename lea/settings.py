fd6c296425b4936a9c020c416c359b9e7f115165
Boguslaw Szczuka <you@example.com>
2014-11-17 14:03:55 +0100

    aa

--- lea/settings.py
+++ lea/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
